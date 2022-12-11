8dcd8390a78987341e9e9f73414b621313575889
Altair Sossai <altairsossai@gmail.com>
2022-12-11 14:32:27 -0300

    screenshot improvement

--- Infrastructure/Helpers/ScreenshotHelper.cs
+++ Infrastructure/Helpers/ScreenshotHelper.cs
@@ -15,17 +15,55 @@ public static class ScreenshotHelper
         var top = rectangle.Top;
         var width = rectangle.Right - left + 1;
         var height = rectangle.Bottom - top + 1;
-        var bitmap = new Bitmap(width, height);
 
-        using var graphics = Graphics.FromImage(bitmap);
+        var desktopWindow = GetDesktopWindow();
+        var windowDc = GetWindowDC(desktopWindow);
+        var compatibleDc = CreateCompatibleDC(windowDc);
+        var compatibleBitmap = CreateCompatibleBitmap(windowDc, width, height);
 
-        graphics.CopyFromScreen(left, top, 0, 0, bitmap.Size, CopyPixelOperation.SourceCopy);
+        SelectObject(compatibleDc, compatibleBitmap);
+        BitBlt(compatibleDc, 0, 0, width, height, windowDc, left, top, 0x00CC0020);
+
+        var intPtr = new IntPtr(compatibleBitmap);
+        var hbitmap = Image.FromHbitmap(intPtr);
+        var bitmap = new Bitmap(hbitmap, hbitmap.Width, hbitmap.Height);
+
+        ReleaseDC(desktopWindow, windowDc);
+        DeleteDC(compatibleDc);
+        DeleteObject(compatibleBitmap);
 
         return bitmap;
     }
 
     [DllImport("user32.dll")]
     private static extern bool GetWindowRect(IntPtr hwnd, ref Rectangle rectangle);
+
+    [DllImport("User32.dll")]
+    private static extern int GetDesktopWindow();
+
+    [DllImport("User32.dll")]
+    private static extern int GetWindowDC(int hWnd);
+
+    [DllImport("GDI32.dll")]
+    private static extern int SelectObject(int hdc, int hgdiobj);
+
+    [DllImport("GDI32.dll")]
+    private static extern bool BitBlt(int hdcDest, int nXDest, int nYDest, int nWidth, int nHeight, int hdcSrc, int nXSrc, int nYSrc, int dwRop);
+
+    [DllImport("GDI32.dll")]
+    private static extern int CreateCompatibleBitmap(int hdc, int nWidth, int nHeight);
+
+    [DllImport("GDI32.dll")]
+    private static extern int CreateCompatibleDC(int hdc);
+
+    [DllImport("User32.dll")]
+    private static extern int ReleaseDC(int hWnd, int hDC);
+
+    [DllImport("GDI32.dll")]
+    private static extern bool DeleteDC(int hdc);
+
+    [DllImport("GDI32.dll")]
+    private static extern bool DeleteObject(int hObject);
 
     private struct Rectangle
     {
