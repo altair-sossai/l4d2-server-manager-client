893d2aa5136ce23b3e3d0ad83603565ec96ea868
Altair Sossai <altairsossai@gmail.com>
2022-12-31 11:59:30 -0300

    Add X-AppId on request header

--- Sdk/Handlers/AuthorizationHeaderHandler.cs
+++ Sdk/Handlers/AuthorizationHeaderHandler.cs
@@ -5,20 +5,22 @@ namespace L4D2AntiCheat.Sdk.Handlers;
 
 public class AuthorizationHeaderHandler : DelegatingHandler
 {
-    private readonly ICurrentUser _currentUser;
+	private readonly ICurrentUser _currentUser;
 
-    public AuthorizationHeaderHandler(ICurrentUser currentUser)
-    {
-        _currentUser = currentUser;
-    }
+	public AuthorizationHeaderHandler(ICurrentUser currentUser)
+	{
+		_currentUser = currentUser;
+	}
 
-    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-    {
-        var accessToken = _currentUser.AccessToken;
+	protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+	{
+		var accessToken = _currentUser.AccessToken;
 
-        if (!string.IsNullOrEmpty(accessToken))
-            request.Headers.Authorization = new AuthenticationHeaderValue("Basic", accessToken);
+		if (!string.IsNullOrEmpty(accessToken))
+			request.Headers.Authorization = new AuthenticationHeaderValue("Basic", accessToken);
 
-        return await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
-    }
+		request.Headers.Add("X-AppId", "FF0DE12E-61E8-4C3B-B382-076710348114");
+
+		return await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
+	}
 }
